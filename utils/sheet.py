d5735654238f660f68b420f94b02f0be84b4e774
juicioplanestucuman-create <juicioplanestucuman@gmail.com>
2025-08-07 11:15:09 -0300

    Update sheet.py

--- utils/sheet.py
+++ utils/sheet.py
@@ -1,1 +1,8 @@
-from sheet import registrar_datos_en_sheet
+# utils/sheet.py
+
+def registrar_datos_en_sheet(datos):
+    # Aquí va todo el código que se conecta con la hoja de cálculo
+    # y registra los datos
+    print("Registrando datos en la hoja de cálculo:", datos)
+    # ... resto del código ...
+    return True
